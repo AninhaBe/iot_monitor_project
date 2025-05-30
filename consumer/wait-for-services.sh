da1f549236e48f1f9b2f1bd7eaf460c59473a4e5
AninhaBe <contato.anabeatrizoliver@gmail.com>
2025-05-30 00:09:31 -0300

    ajustando shell do consumer

--- consumer/wait-for-services.sh
+++ consumer/wait-for-services.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-echo "Aguardando PostgreSQL em $DB_HOST:$DB_PORT ..."
-while ! nc -z "$DB_HOST" "$DB_PORT"; do
-  echo "$(date) :: PostgreSQL ainda não está pronto ..."
-  sleep 1
-done
-echo "PostgreSQL está pronto!"
-
 echo "Aguardando Kafka em $KAFKA_BOOTSTRAP_SERVERS ..."
 KAFKA_HOST=$(echo "$KAFKA_BOOTSTRAP_SERVERS" | cut -d':' -f1)
 KAFKA_PORT=$(echo "$KAFKA_BOOTSTRAP_SERVERS" | cut -d':' -f2)
